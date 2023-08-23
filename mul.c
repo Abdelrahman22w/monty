ce72199f874f44dd720427b9e23a2ecf261bf701
hadiglitch10 <hadiunknownglitch@gmail.com>
2023-08-23 20:11:41 +0300

    hadi 22/8 9:29

---
+++ mul.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * mul - sub top element from its prev
+ *
+ * @head: stack header
+ * @counter: node counter
+ *
+ * Return: no
+*/
+
+void mul(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		goto cleanup;
+		return;
+	}
+	/*--------------------------------------*/
+
+	(*head)->next->n *= (*head)->n;
+	pop(head, counter);
+
+	/*-------------------------------------*/
+	cleanup:
+	fclose(bus.file);
+	free(bus.content);
+	free_stack(*head);
+	exit(EXIT_FAILURE);
+}
